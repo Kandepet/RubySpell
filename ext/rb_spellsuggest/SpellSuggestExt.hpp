47a4072b7cc4bf15a621e555e8d1a12275fe0627
Hacker Labs <hacker@hackerlabs.co>
2012-06-20 00:18:18 +0000

    Removed unnecessary files

--- ext/rb_spellsuggest/SpellSuggestExt.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __SPELL_SUGGEST_H__
-#define __SPELL_SUGGEST_H__
-
-#include <ruby.h>
-
-VALUE mSpell;
-VALUE cSpellSuggest;
-
-#endif //__SPELL_SUGGEST_H__
--- ext/rb_spellsuggest/extconf.rb
+++
@@ -1,7 +0,0 @@
-require "mkmf"
-
-$CFLAGS = "-D_GNU_SOURCE -Wall -std=c++0x"
-
-have_header("ruby.h")
-have_library("stdc++")
-create_makefile("spellsuggest")
